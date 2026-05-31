4782f7562c5bac767a38f6b8c072ab24b6ae03ed
pppppengJ <153730553+pppppengJ@users.noreply.github.com>
2026-05-31 10:09:07 +0900

    Add files via upload

---
+++ Sonar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepEchoGame
+{
+    class Sonar
+    {
+        private Map map;
+
+        public Sonar(Map map)
+        {
+            this.map = map;
+        }
+
+        public void Scan(int index) //구역의 몬스터 존재 스캔
+        {
+            if (index < 1 || index > map.Zones.Count)
+            {
+                Zone target = map.Zones[index - 1];
+
+                Console.WriteLine("=== 소나 스캔 ===");
+
+                if (target.HasMonster)
+                    Console.WriteLine($"[{target.Name}] : 위협적인 반응이 감지됩니다.");
+                else
+                    Console.WriteLine($"[{target.Name}] : 외부 반응이 느껴지지 않습니다.");
+            }
+            // 못 찾았을 때
+            else
+            {
+                Console.WriteLine("존재하지 않는 구역입니다. 다시 시도해 주세요.");
+                return;
+            }
+        }
+    }
+}
